fe6769d7478546cfe7a33a17b853d67fbb16597d
Lucas Andrianina ANDRIAMANGA <hei.lucas.13@gmail.com>
2026-03-20 08:38:09 +0300

    Init commit

---
+++ src/main/java/school/hei/manipreqetresphttpwithspring/ManipReqEtRespHttpWithSpringApplication.java
@@ -0,0 +1,13 @@
+package school.hei.manipreqetresphttpwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManipReqEtRespHttpWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManipReqEtRespHttpWithSpringApplication.class, args);
+    }
+
+}
